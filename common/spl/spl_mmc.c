xobs/u-boot-novena-spl

Clean up the spl_mmc handling so that it's compatible with other boards
The CuBox-i wants to fall through and try loading from either of FAT or RAW,
whichever works.  That's fine, but don't break compatibility with other
boards which expect to be configured to load from one or the other.

Signed-off-by: Steve Langasek <vorlon@debian.org>

common/spl/spl_mmc.c
@@ -142,30 +142,51 @@ void spl_mmc_load_image(void)
 	}
 
 
-#ifdef CONFIG_SPL_FAT_SUPPORT
-	/* FAT filesystem */
-	err = fat_register_device(&mmc->block_dev,
-			  CONFIG_SYS_MMC_SD_FAT_BOOT_PARTITION);
-	/*if (err) {
-		printf("spl: fat register err - %d\n", err);
-	}*/
+	boot_mode = spl_boot_mode();
+	switch (boot_mode) {
+		case MMCSD_MODE_RAW:
+		case MMCSD_MODE_ANY:
 #ifdef CONFIG_SPL_OS_BOOT
-	if (spl_start_uboot() || mmc_load_image_fat_os(mmc))
+		case MMCSD_MODE_FAT:
 #endif
-	err = mmc_load_image_fat(mmc, CONFIG_SPL_FAT_LOAD_PAYLOAD_NAME);
+			break;
+		default:
+#ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
+			puts("spl: wrong MMC boot mode\n");
 #endif
+			hang();
+			break;
+	}
 
-	if (err) {
-		printf("Load image from RAW...\n");
+	if (boot_mode == MMCSD_MODE_RAW || boot_mode == MMCSD_MODE_ANY) {
+		debug("boot mode - RAW\n");
 #ifdef CONFIG_SPL_OS_BOOT
 		if (spl_start_uboot() || mmc_load_image_raw_os(mmc))
 #endif
-		err = mmc_load_image_raw(mmc, CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR);
+		err = mmc_load_image_raw(mmc,
+					 CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR);
+	}
+#ifdef CONFIG_SPL_FAT_SUPPORT
+	if (boot_mode == MMCSD_MODE_FAT || (boot_mode == MMCSD_MODE_ANY && err))
+	{
+		debug("boot mode - FAT\n");
+
+		err = fat_register_device(&mmc->block_dev,
+					  CONFIG_SYS_MMC_SD_FAT_BOOT_PARTITION);
 		if (err) {
 #ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
-			printf("spl: wrong MMC boot mode\n");
+			printf("spl: fat register err - %d\n", err);
 #endif
 			hang();
 		}
+
+#ifdef CONFIG_SPL_OS_BOOT
+		if (spl_start_uboot() || mmc_load_image_fat_os(mmc))
+#endif
+		err = mmc_load_image_fat(mmc, CONFIG_SPL_FAT_LOAD_PAYLOAD_NAME);
 	}
+#endif
+
+	if (err)
+		hang();
 }

vorlonofportland/u-boot

common/spl/spl_mmc.c
@@ -92,19 +92,27 @@ void spl_mmc_load_image(void)
 		hang();
 	}
 
-
-#ifdef CONFIG_SPL_FAT_SUPPORT
-	/* FAT filesystem */
-#ifdef CONFIG_SPL_OS_BOOT
-	if (spl_start_uboot() || spl_load_image_fat_os(&mmc->block_dev,
-							CONFIG_SYS_MMC_SD_FAT_BOOT_PARTITION))
-#endif
-		err = spl_load_image_fat(&mmc->block_dev,
-					CONFIG_SYS_MMC_SD_FAT_BOOT_PARTITION,
-					CONFIG_SPL_FAT_LOAD_PAYLOAD_NAME);
-#endif
+	boot_mode = spl_boot_mode();
+	switch (boot_mode) {
+		case MMCSD_MODE_RAW:
+		case MMCSD_MODE_ANY:
 #ifdef CONFIG_SUPPORT_EMMC_BOOT
-	if (err) {
+		case MMCSD_MODE_EMMCBOOT:
+#endif
+#ifdef CONFIG_SPL_FAT_SUPPORT
+		case MMCSD_MODE_FAT:
+#endif
+			break;
+		default:
+#ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
+			puts("spl: wrong MMC boot mode\n");
+#endif
+			hang();
+			break;
+	}
+
+#ifdef CONFIG_SUPPORT_EMMC_BOOT
+	if (boot_mode == MMCSD_MODE_EMMCBOOT) {
 		/*
 		 * We need to check what the partition is configured to.
 		 * 1 and 2 match up to boot0 / boot1 and 7 is user data
@@ -123,19 +131,30 @@ void spl_mmc_load_image(void)
 		}
 	}
 #endif
-
-	if (err) {
-		printf("Load image from RAW...\n");
+	if (boot_mode == MMCSD_MODE_RAW || boot_mode == MMCSD_MODE_ANY
+	    || boot_mode == MMCSD_MODE_EMMCBOOT)
+	{
+		debug("boot mode - RAW\n");
 #ifdef CONFIG_SPL_OS_BOOT
 		if (spl_start_uboot() || mmc_load_image_raw_os(mmc))
 #endif
 		err = mmc_load_image_raw(mmc,
 			CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR);
-		if (err) {
-#ifdef CONFIG_SPL_LIBCOMMON_SUPPORT
-			printf("spl: wrong MMC boot mode\n");
-#endif
-			hang();
-		}
 	}
+#ifdef CONFIG_SPL_FAT_SUPPORT
+	if (boot_mode == MMCSD_MODE_FAT || (boot_mode == MMCSD_MODE_ANY && err))
+	{
+		debug("boot mode - FAT\n");
+#ifdef CONFIG_SPL_OS_BOOT
+		if (spl_start_uboot() || spl_load_image_fat_os(&mmc->block_dev,
+								CONFIG_SYS_MMC_SD_FAT_BOOT_PARTITION))
+#endif
+		err = spl_load_image_fat(&mmc->block_dev,
+					CONFIG_SYS_MMC_SD_FAT_BOOT_PARTITION,
+					CONFIG_SPL_FAT_LOAD_PAYLOAD_NAME);
+	}
+#endif
+
+	if (err)
+		hang();
 }